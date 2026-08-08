d20e47cebade36a8c6ae1d6d31994cd6a087fedf
Abner <abnercharuc24011063@gmail.com>
2026-08-07 18:15:38 -0600

    Creando estructura de layouts para el diseño base

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Friends Burgers</title>
+</head>
+<body>
+    <h1>¡Bienvenido a Friends Burgers!</h1>
+    @yield('content')
+</body>
+</html>
